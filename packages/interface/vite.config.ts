5317ded213c81f515f59c3e8d54306b5eb997523
noy4 <naoyan418@gmail.com>
2022-11-11 02:36:50 +0700

    init daisy

--- packages/interface/vite.config.ts
+++ packages/interface/vite.config.ts
@@ -1,8 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import type { UserConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import { presetUno, transformerDirectives } from 'unocss'
+import presetDaisy from 'unocss-preset-daisy'
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
-};
+  plugins: [
+    UnoCSS({
+      transformers: [transformerDirectives()],
+      presets: [presetUno(), presetDaisy()],
+    }),
+    sveltekit(),
+  ],
+}
 
-export default config;
+export default config
