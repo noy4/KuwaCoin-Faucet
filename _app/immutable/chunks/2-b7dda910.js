d9e1349e292deb9985cdfee49fdc21e3eaff709e
noy4 <noy4@users.noreply.github.com>
2022-11-22 06:44:01 +0000

    deploy: a472cd44f7ab46e0a11a5ae4743e163bd3494999

---
+++ _app/immutable/chunks/2-b7dda910.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fbd71a6c.js";export{t as component};
--- _app/immutable/chunks/2-c8ab93f0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-4bde9d7c.js";export{t as component};
